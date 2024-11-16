adb9e0f6a3479857cd94a0b43d12668665808b42
caiohenriquesa1@gmail.com <caiohenriquesa1@gmail.com>
2024-11-16 12:52:55 -0300

    warmup: begin calculator and add test

--- owned_calculator/programs/owned_calculator/src/lib.rs
+++ owned_calculator/programs/owned_calculator/src/lib.rs
@@ -7,9 +7,40 @@ pub mod owned_calculator {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        msg!("Welcome to Solana!: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn add(ctx: Context<Initialize>, a:u64, b:u64) -> Result<()> {
+        msg!("The add of a + b is {:?}", a.checked_add(b).unwrap());
+        Ok(())
+    }
+
+    pub fn sub(ctx: Context<Initialize>, a:u64, b:u64) -> Result<()> {
+        msg!("The sub of a - b is {:?}", a.checked_sub(b).unwrap());
+        Ok(())
+    }
+
+    pub fn div(ctx: Context<Initialize>, a:u64, b:u64) -> Result<()> {
+        if b == 0 { return err!(InvalidArg::DivisionByZero); }
+        msg!("The div of a/b is {}", a/b);
+        Ok(())
+    }
+
+    pub fn multipleAdd(ctx: Context<Initialize>, numbers: Vec<u64>) -> Result<()> {
+        let mut sum:u64 = 0;
+        for num in &numbers {
+            sum += num;
+        }
+        msg!("The total sum is {}", sum);
+        Ok(())
+    }
+}
+
+#[error_code]
+pub enum InvalidArg {
+    #[msg("CannotDivideByZero")]
+    DivisionByZero,
 }
 
 #[derive(Accounts)]
--- owned_calculator/tests/owned_calculator.ts
+++ owned_calculator/tests/owned_calculator.ts
@@ -13,4 +13,10 @@ describe("owned_calculator", () => {
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("Simple add two numbers", async () => {
+    // add()
+    const tx = await program.methods.add(new anchor.BN(1), new anchor.BN(2)).rpc();
+    console.log("tx sig", tx);
+  });
 });
