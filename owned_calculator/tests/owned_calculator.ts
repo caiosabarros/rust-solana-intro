e4e19740e43865651e719195908a1cac9d8d8ddd
caiohenriquesa1@gmail.com <caiohenriquesa1@gmail.com>
2024-11-16 13:43:59 -0300

    add positive and negative tests for mul && add test for multiple_add

--- owned_calculator/tests/owned_calculator.ts
+++ owned_calculator/tests/owned_calculator.ts
@@ -1,12 +1,14 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { OwnedCalculator } from "../target/types/owned_calculator";
+import { assert } from "chai";
 
 describe("owned_calculator", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.OwnedCalculator as Program<OwnedCalculator>;
+  let keyPair = anchor.web3.Keypair.generate(); // generate random keypair
 
   it("Is initialized!", async () => {
     // Add your test here.
@@ -22,7 +24,28 @@ describe("owned_calculator", () => {
 
   it("Owner multiplication", async () => {
     // mul
-    const tx = await program.methods.mul(new anchor.BN(2), new anchor.BN(3)).accounts({signerAccount: program.provider.publicKey}).rpc();
+    const tx = await program.methods.mul(new anchor.BN(2), new anchor.BN(3)).accounts({ signerAccount: program.provider.publicKey }).rpc();
     console.log("tx sig", tx);
   });
+
+  it("Not owner cannot multiply", async () => {
+    try {
+      const tx = await program.methods.mul(new anchor.BN(2), new anchor.BN(3)).accounts({
+        signerAccount: keyPair.publicKey
+      })
+        .signers([keyPair])
+        .rpc();
+    } catch (_err) {
+      assert.isTrue(_err instanceof anchor.AnchorError);
+      const err: anchor.AnchorError = _err;
+      const message = "You are a strange, not the owner!";
+      assert.strictEqual(err.error.errorMessage, message);
+    }
+  });
+
+  it("Add multiple", async () => {
+    const tx = await program.methods.multipleAdd([new anchor.BN(1), new anchor.BN(2), new anchor.BN(1e3)]).rpc();
+    console.log("tx sig", tx);
+  });
+
 });
